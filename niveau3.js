df94d0e9e05fba64abbf69d161eb1525a56795f0
LunaSimoes <59608391+LunaSimoes@users.noreply.github.com>
2020-03-17 17:00:53 +0100

    fantome move + niveau 3

--- niveau3.js
+++ niveau3.js
@@ -5,7 +5,7 @@ var config = {
 	physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 },
+            gravity: { y: 200 },
             debug: true
         }
     },
@@ -24,52 +24,24 @@ function init(){
 	var platforms;
 	var player;
 	var stars;
-	var fantome;
 }
 function preload(){
-	this.load.image('background','assets/fondpong.png');	
-	this.load.image('sol','assets/solpacman.png');
-	this.load.image('sol2','assets/solpac2.png');
-	this.load.image('sol3','assets/solpac3.png');
-	this.load.image('sol4','assets/solpac4.png');
+	this.load.image('background','assets/sky.png');	
+	this.load.image('sol','assets/kassos.png');
 	this.load.image('stars', 'assets/donnee.png');
-	this.load.image('fantome','assets/fantome.png');
 	this.load.spritesheet('perso','assets/robott.png',{frameWidth: 31, frameHeight: 47});
 }
 function create(){
 	this.add.image(400,50,'background');
 	
 	platforms = this.physics.add.staticGroup();
-	platforms.create(60,750,'sol').setScale(1).refreshBody();
-	platforms.create(470,750,'sol');
-	platforms.create(850,750,'sol');
-	platforms.create(60,-10,'sol');
-	platforms.create(470,-10,'sol');
-	platforms.create(850,-10,'sol');
-	platforms.create(500,600,'sol2');
-	platforms.create(500,200,'sol2');
-	platforms.create(500,550,'sol3');
-	platforms.create(500,150,'sol3');
-	platforms.create(-100,500,'sol2');
-	platforms.create(-100,200,'sol2');
-	platforms.create(1100,500,'sol2');
-	platforms.create(1100,200,'sol2');
-	platforms.create(300,420,'sol4');
-	platforms.create(700,420,'sol4');
-
-
-	fantome = this.physics.add.group();
-	fantome.create(200, 300, 'fantome');
-	fantome.create(800, 300, 'fantome');
-	 this.physics.add.collider(fantome, platforms);
-
-	 
-	fantome.children.iterate(function (child){
-		child.setBounceY(Phaser.Math.FloatBetween(1, 1.3));
-	});
+	platforms.create(60,600,'sol').setScale(1).refreshBody();
+	platforms.create(470,600,'sol');
+	platforms.create(850,600,'sol');
 	
-	player = this.physics.add.sprite(500,670,'perso');
+	player = this.physics.add.sprite(100,450,'perso');
 	player.setCollideWorldBounds(true);
+	player.setBounce(0.2);
 	this.physics.add.collider(player,platforms);
 	
 	cursor = this.input.keyboard.createCursorKeys();
@@ -77,7 +49,7 @@ function create(){
 	stars = this.physics.add.group({
 		key: 'stars',
 		repeat:0,
-		setXY: {x:500, y:400, stepX:70 }
+		setXY: {x:500, y:0, stepX:70 }
 	})
 	 this.physics.add.collider(stars, platforms);
 	 this.physics.add.overlap(player,stars,collectStar, null, this);
@@ -113,14 +85,12 @@ function update(){
 		player.anims.play('left',true);
 		player.setVelocityX(-190);
 		player.setFlipX(false);
-		fantome.setVelocityY(-150);
 	}
 	
 	else if(cursor.right.isDown) {
 		player.anims.play('left',true);
 		player.setVelocityX(190);
-		player.setFlipX(true);
-		fantome.setVelocityY(150);		
+		player.setFlipX(true);	
 	}
 	
 	else {
@@ -128,11 +98,11 @@ function update(){
 		player.setVelocityX(0);
 	}
 
-	if(cursor.up.isDown){
+	if(cursor.up.isDown && player.body.touching.down){
 		player.setVelocityY(-200);
 	}
 	
 	if(cursor.down.isDown){
-		player.setVelocityY(200);
+		player.setVelocityY(400);
 	}
 }
